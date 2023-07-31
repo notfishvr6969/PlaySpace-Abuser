32e98614ff824a2f293ebb6f7da93c21adcd2c37
notfishvr6969 <136778455+notfishvr6969@users.noreply.github.com>
2023-07-31 14:20:24 -0700

    Add files via upload

--- PlaySpace Abuser/PlaySpace Abuser.cs
+++ PlaySpace Abuser/PlaySpace Abuser.cs
@@ -1,26 +1,24 @@
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR;
 using HarmonyLib;
-using BepInEx;
 using GorillaLocomotion;
 
-namespace PlayerMovementController
+namespace PlaySpaceAbuser
 {
     [HarmonyPatch(typeof(Player))]
-    [HarmonyPatch("Update", MethodType.Normal)]
+    [HarmonyPatch("LateUpdate", MethodType.Normal)]
     public class PlayerMovementControllerPatch
     {
-        private const float MovementSpeed = 10f;
         private static void Update()
         {
-            bool isGripButtonPressed = false;
-            List<InputDevice> devices = new List<InputDevice>();
-            InputDevices.GetDevicesWithCharacteristics(InputDeviceCharacteristics.HeldInHand | InputDeviceCharacteristics.Controller | InputDeviceCharacteristics.Right, devices);
-
-            if (devices.Count > 0 && devices[0].TryGetFeatureValue(CommonUsages.gripButton, out isGripButtonPressed) && isGripButtonPressed)
+            Prefix();
+        }
+        private static void Prefix()
+        {
+            InputDevices.GetDeviceAtXRNode(XRNode.RightHand).TryGetFeatureValue(CommonUsages.gripButton, out bool rightGripButton);
+            if (rightGripButton)
             {
-                Player.Instance.transform.position += Player.Instance.bodyCollider.transform.forward * Time.deltaTime * MovementSpeed;
+                Player.Instance.transform.position += Player.Instance.bodyCollider.transform.forward * Time.deltaTime * 5f;
             }
         }
     }
